aa48414b6f616adb8aa427dc07fd51a9bbbc150d
anchvy <anchvy@Macintosh.lan>
2019-09-14 21:04:03 +0900

    initial project

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  padding: 10px;
+`
+
+const App = () => (
+  <Wrapper>
+    <p>ww</p>
+  </Wrapper>
+)
+
+export default App
